bb5e34555b015df318c78798bb9dcd4bf1c25c27
Polrii <mcpekapeque@gmail.com>
2025-02-03 21:35:33 +0100

    Started trying multi-detection

---
+++ ObjectDetector.py
@@ -0,0 +1,3 @@
+from ultralytics import YOLO
+model = YOLO("yolov8n.pt")
+model.train(data="C:/python_work/AI/DWG2CAD/TrainingData/NumsDataset.v5i.yolov8/data.yaml", epochs=50)
---
+++ TrainingData/NumsDataset.v5i.openai/README.dataset.txt
@@ -0,0 +1,6 @@
+# NumsDataset > 2025-02-03 6:41pm
+https://universe.roboflow.com/polriis-workspace/numsdataset
+
+Provided by a Roboflow user
+License: MIT
+
