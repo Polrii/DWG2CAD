b6c2f9aa0eb664ff4558a3cb47d967468ef647b5
Polrii <mcpekapeque@gmail.com>
2025-02-06 22:44:48 +0100

    Trained up to epoch10000

--- CSV_Formatter.py
+++ CSV_Formatter.py
@@ -1,15 +1,17 @@
 import csv
 
 def convert_csv(file_path):
+    # Open the file and modify the format
     with open(file_path, 'r', newline='', encoding='utf-8') as infile:
         reader = csv.reader(infile)
         rows = [[col.replace('.', ',') for col in row] for row in reader]
     
-    with open(file_path, 'w', newline='', encoding='utf-8') as outfile:
+    # Save the new file
+    with open(f"{file_path.replace('results.csv', 'modified_results.csv')}", 'w', newline='', encoding='utf-8') as outfile:
         for row in rows:
             outfile.write('\t'.join(row) + '\n')
 
 if __name__ == "__main__":
-    file_path = "C:/python_work/AI/DWG2CAD/runs/detect/train3/results.csv"  # Change this to your file
+    file_path = "C:/python_work/AI/DWG2CAD/runs/detect/train4/results.csv"  # Change this to your file
     convert_csv(file_path)
     print(f"Modified file saved as {file_path}")
