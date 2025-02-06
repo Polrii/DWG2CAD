12d39b0c2a378b0053a775285b6ca343e97aef25
Polrii <mcpekapeque@gmail.com>
2025-02-06 08:26:39 +0100

    Trained until epoch2700. Continue training

--- ObjectDetectorTrain.py
+++ ObjectDetectorTrain.py
@@ -2,12 +2,14 @@ from ultralytics import YOLO
 
 
 # Training from the start
+"""
 model = YOLO("yolov8n.pt")  # Load pre-trained YOLO model
 model.train(data="C:/python_work/AI/DWG2CAD/TrainingData/NumsDataset.v6i.yolov8/data.yaml", epochs=1000000, save_period=100, patience=0)
+"""
 
 
 # Training from last checkpoint
-"""
-model = YOLO("runs/train/exp/weights/last.pt")  # Load last checkpoint
-model.train(resume=True)"""
-
+""""""
+model = YOLO("C:/python_work/AI/DWG2CAD/runs/detect/train4/weights/last.pt")  # Load last checkpoint
+model.train(resume=True)
+""""""
